1b4fb2b0621b1ebf998857d3fdd8d9890a8b1306
David Pedersen <david.pdrsn@gmail.com>
2013-07-10 11:59:17 +0200

    Wrap code in document.ready and add js to create table headers

--- responsive-table.js
+++ responsive-table.js
@@ -1,19 +1,41 @@
-$('table').each(function(){
-  var table = $(this);
-  var headers = [];
-  table.find('th').each(function(){
-    headers.push( $(this).text() );
+$(function(){
+
+  $('table').each(function(){
+    if (!$(this).find('thead').length) {
+
+      var headers = [];
+
+      $(this).find('tbody tr').first().find('td').each(function(){
+        headers.push($(this).text());
+      }).parents('tr').remove();
+
+      $(this).prepend('<thead><tr></tr></thead>');
+
+      for (var i = headers.length - 1; i >= 0; i--) {
+        $(this).find('thead tr').append('<th>' + headers[i] + '</th>');
+      };
+
+    }
   });
 
-  var cells = table.find('tbody tr td');
-  var cellsCount = table.find('thead th').length - 1;
-  var a = 0;
-  for (var i = 0; i < cells.length; i += 1) {
-    cells[i].setAttribute('data-header', headers[a]);
-    if (a === cellsCount) {
-      a = 0;
-    } else {
-      a++;
+  $('table').each(function(){
+    var table = $(this);
+    var headers = [];
+    table.find('th').each(function(){
+      headers.push( $(this).text() );
+    });
+
+    var cells = table.find('tbody tr td');
+    var cellsCount = table.find('thead th').length - 1;
+    var a = 0;
+    for (var i = 0; i < cells.length; i += 1) {
+      cells[i].setAttribute('data-header', headers[a]);
+      if (a === cellsCount) {
+        a = 0;
+      } else {
+        a++;
+      }
     }
-  }
+  });
+
 });
